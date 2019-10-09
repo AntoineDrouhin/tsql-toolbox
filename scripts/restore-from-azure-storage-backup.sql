3f3cba62780a7625008f6517dbcebd148aab1c4d
antoine drouhin <antoine.drouhin@gmail.com>
2019-10-09 16:56:25 +0200

    update restore script to allow to select folder

--- scripts/restore-from-azure-storage-backup.sql
+++ scripts/restore-from-azure-storage-backup.sql
@@ -8,17 +8,28 @@ DECLARE @Date AS NVARCHAR(25)
    ,@StorageAccountName AS VARCHAR(MAX)
    ,@SASKey AS VARCHAR(MAX)
    ,@DatabaseName AS SYSNAME
-   ,@BakFileName as VARCHAR(MAX);
+   ,@BakFileName AS VARCHAR(MAX)
+   ,@DestinationFolderData AS VARCHAR(MAX)
+   ,@DestinationFolderLog AS VARCHAR(MAX);
 SELECT @Date = REPLACE(REPLACE(REPLACE(REPLACE(CONVERT(VARCHAR, GETDATE(), 100), '  ', '_'), ' ', '_'), '-', '_'), ':', '_');
-SELECT @StorageAccountName = ''; --- Find this from Azure Portal
-SELECT @ContainerName = ''; --- Find this from Azure Portal
-SELECT @SASKey = ''; --- Find this from Azure Portal
+
+--- Find this from Azure Portal
+SELECT @StorageAccountName = '';
+--- Find this from Azure Portal
+SELECT @ContainerName = '';
+--- Find this from Azure Portal
+SELECT @SASKey = '';
+
 SELECT @DatabaseName = '';
 SELECT @BakFileName =  '';
+-- Optional -- conclude by file separator (/ or \ depending on the os)
+-- Required when restoring a backup made on windows on linux or the other way around
+SELECT @DestinationFolderData = 'F:\data\';
+SELECT @DestinationFolderLog = 'F:\log\'
 IF EXISTS (
        SELECT *
-        FROM sys.credentials
-        WHERE name = 'https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName 
+FROM sys.credentials
+WHERE name = 'https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName 
 )
 BEGIN
     SELECT @TSQL = 'DROP CREDENTIAL [https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName + ']'
@@ -27,4 +38,12 @@ END
 SELECT @TSQL = 'CREATE CREDENTIAL [https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName + '] WITH IDENTITY = ''SHARED ACCESS SIGNATURE'', SECRET = ''' + REPLACE(@SASKey, '?sv=', 'sv=') + ''';'
 EXEC (@TSQL)
 SELECT @TSQL = 'RESTORE DATABASE [' + @DatabaseName + '] FROM URL = ''https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName+ '/'+ @BakFileName + ''''
+IF(@DestinationFolderData != '')
+BEGIN
+    SELECT @TSQL = @TSQL + ' WITH MOVE ''' + @DatabaseName + ''' TO ''' + @DestinationFolderData + '\' + @databaseName + '.mdf'''
+
+    IF(@DestinationFolderLog != '')
+    SELECT @TSQL = @TSQL + ' ,MOVE ''' + @DatabaseName + '_log'' TO ''' + @DestinationFolderData + '\' + @databaseName + '_log.ldf'''
+
+END
 EXEC (@TSQL)
