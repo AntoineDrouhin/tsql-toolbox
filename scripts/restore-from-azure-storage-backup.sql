7f5b48624b5f81e2834421dd6a7e108eb1708d0b
antoine drouhin <antoine.drouhin@gmail.com>
2019-10-09 17:38:08 +0200

    Fix script restore from azure

--- scripts/restore-from-azure-storage-backup.sql
+++ scripts/restore-from-azure-storage-backup.sql
@@ -21,7 +21,7 @@ SELECT @ContainerName = '';
 SELECT @SASKey = '';
 
 SELECT @DatabaseName = '';
-SELECT @BakFileName =  '';
+SELECT @BakFileName = '';
 -- Optional -- conclude by file separator (/ or \ depending on the os)
 -- Required when restoring a backup made on windows on linux or the other way around
 SELECT @DestinationFolderData = 'F:\data\';
@@ -40,10 +40,10 @@ EXEC (@TSQL)
 SELECT @TSQL = 'RESTORE DATABASE [' + @DatabaseName + '] FROM URL = ''https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName+ '/'+ @BakFileName + ''''
 IF(@DestinationFolderData != '')
 BEGIN
-    SELECT @TSQL = @TSQL + ' WITH MOVE ''' + @DatabaseName + ''' TO ''' + @DestinationFolderData + '\' + @databaseName + '.mdf'''
+    SELECT @TSQL = @TSQL + ' WITH MOVE ''' + @DatabaseName + ''' TO ''' + @DestinationFolderData + @databaseName + '.mdf'''
 
     IF(@DestinationFolderLog != '')
-    SELECT @TSQL = @TSQL + ' ,MOVE ''' + @DatabaseName + '_log'' TO ''' + @DestinationFolderData + '\' + @databaseName + '_log.ldf'''
+    SELECT @TSQL = @TSQL + ' ,MOVE ''' + @DatabaseName + '_log'' TO ''' + @DestinationFolderLog + @databaseName + '_log.ldf'''
 
 END
 EXEC (@TSQL)
