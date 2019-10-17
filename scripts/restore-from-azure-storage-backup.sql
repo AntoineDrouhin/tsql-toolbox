e2f48bfe1667815ad48dce230360e281209dc3ea
antoine drouhin <antoine.drouhin@gmail.com>
2019-10-17 20:02:24 +0200

    Support restoring database from multiple bak files

--- scripts/restore-from-azure-storage-backup.sql
+++ scripts/restore-from-azure-storage-backup.sql
@@ -1,5 +1,8 @@
 --- ============================================================================= ---
 -- Restore from URL (using SAS Token) :
+
+
+-- For this script to work, bak files are supposed to end by _0.bak , _1.bak etc...
 --- ============================================================================= ---
 
 DECLARE @Date AS NVARCHAR(25)
@@ -9,6 +12,7 @@ DECLARE @Date AS NVARCHAR(25)
    ,@SASKey AS VARCHAR(MAX)
    ,@DatabaseName AS SYSNAME
    ,@BakFileName AS VARCHAR(MAX)
+   ,@NumberOfBakFiles AS INTEGER
    ,@DestinationFolderData AS VARCHAR(MAX)
    ,@DestinationFolderLog AS VARCHAR(MAX);
 SELECT @Date = REPLACE(REPLACE(REPLACE(REPLACE(CONVERT(VARCHAR, GETDATE(), 100), '  ', '_'), ' ', '_'), '-', '_'), ':', '_');
@@ -21,11 +25,23 @@ SELECT @ContainerName = '';
 SELECT @SASKey = '';
 
 SELECT @DatabaseName = '';
+
+--- Bak file name prefix, without the file end or the extension. (real filename must finish by _[0-9] )
+--- Ex myfile_0.bak => myfile
 SELECT @BakFileName = '';
+
+SELECT @NumberOfBakFiles = 1;
+
 -- Optional -- conclude by file separator (/ or \ depending on the os)
 -- Required when restoring a backup made on windows on linux or the other way around
 SELECT @DestinationFolderData = 'F:\data\';
 SELECT @DestinationFolderLog = 'F:\log\'
+-- SELECT @DestinationFolderData = '/var/opt/mssql/data/';
+-- SELECT @DestinationFolderLog = '/var/opt/mssql/data/';
+
+
+--------------------------------- SCRIPT --------------------------------------
+
 IF EXISTS (
        SELECT *
 FROM sys.credentials
@@ -37,7 +53,20 @@ BEGIN
 END
 SELECT @TSQL = 'CREATE CREDENTIAL [https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName + '] WITH IDENTITY = ''SHARED ACCESS SIGNATURE'', SECRET = ''' + REPLACE(@SASKey, '?sv=', 'sv=') + ''';'
 EXEC (@TSQL)
-SELECT @TSQL = 'RESTORE DATABASE [' + @DatabaseName + '] FROM URL = ''https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName+ '/'+ @BakFileName + ''''
+
+
+SELECT @TSQL = 'RESTORE DATABASE [' + @DatabaseName + '] FROM URL = ''https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName+ '/'+ @BakFileName + '_0.bak'''
+
+DECLARE @fileCounter integer = 1;
+
+WHILE (@fileCounter < @NumberOfBakFiles)
+BEGIN
+    SELECT @TSQL += ' , URL = ''https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName+ '/'+ @BakFileName + '_'+ CAST(@fileCounter as varchar(10)) + '.bak'''
+    SET @fileCounter = @fileCounter + 1
+END
+
+
+
 IF(@DestinationFolderData != '')
 BEGIN
     SELECT @TSQL = @TSQL + ' WITH MOVE ''' + @DatabaseName + ''' TO ''' + @DestinationFolderData + @databaseName + '.mdf'''
@@ -47,3 +76,4 @@ BEGIN
 
 END
 EXEC (@TSQL)
+
