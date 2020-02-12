6230348e336338646e967082a3719003e43fbddd
antoine drouhin <antoine.drouhin@gmail.com>
2020-02-12 09:20:35 +0100

    [backup] Restore with rename

---
+++ scripts/backup-restore/restore-from-azure-storage-with-rename.sql
@@ -0,0 +1,83 @@
+--- ============================================================================= ---
+-- Restore from URL (using SAS Token) :
+-- For this script to work, bak files are supposed to end by _0.bak , _1.bak etc...
+--- ============================================================================= ---
+
+DECLARE @Date AS NVARCHAR(25)
+   ,@TSQL AS NVARCHAR(MAX)
+   ,@ContainerName AS NVARCHAR(MAX)
+   ,@StorageAccountName AS VARCHAR(MAX)
+   ,@SASKey AS VARCHAR(MAX)
+   ,@DatabaseName AS SYSNAME
+   ,@DatabaseNewName AS SYSNAME
+   ,@BakFileName AS VARCHAR(MAX)
+   ,@NumberOfBakFiles AS INTEGER
+   ,@DestinationFolderData AS VARCHAR(MAX) = ''
+   ,@DestinationFolderLog AS VARCHAR(MAX);
+SELECT @Date = REPLACE(REPLACE(REPLACE(REPLACE(CONVERT(VARCHAR, GETDATE(), 100), '  ', '_'), ' ', '_'), '-', '_'), ':', '_');
+--- Find this from Azure Portal
+SELECT @StorageAccountName = 'sciencebitch';
+
+--- Find this from Azure Portal
+SELECT @ContainerName = '';
+SELECT @SASKey = '';
+SELECT @DatabaseName = '';
+-------------------------------
+
+SELECT @DatabaseNewName = '';
+
+--- Bak file name prefix, without the file end or the extension. (real filename must finish by _[0-9] )
+--- Ex myfile_0.bak => myfile
+SELECT @BakFileName = '';
+
+SELECT @NumberOfBakFiles = 1;
+
+-- Optional -- conclude by file separator (/ or \ depending on the os)
+-- Required when restoring a backup made on windows on linux or the other way around
+SELECT @DestinationFolderData = 'F:\data\';
+SELECT @DestinationFolderLog = 'F:\log\';
+-- SELECT @DestinationFolderData = '/var/opt/mssql/data/';
+-- SELECT @DestinationFolderLog = '/var/opt/mssql/data/';
+
+
+--------------------------------- SCRIPT --------------------------------------
+
+IF EXISTS (
+       SELECT *
+FROM sys.credentials
+WHERE name = 'https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName 
+)
+BEGIN
+    SELECT @TSQL = 'DROP CREDENTIAL [https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName + ']'
+    EXEC (@TSQL)
+END
+SELECT @TSQL = 'CREATE CREDENTIAL [https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName + '] WITH IDENTITY = ''SHARED ACCESS SIGNATURE'', SECRET = ''' + REPLACE(@SASKey, '?sv=', 'sv=') + ''';'
+EXEC (@TSQL)
+
+
+SELECT @TSQL = 'RESTORE DATABASE [' + @DatabaseNewName + '] FROM URL = ''https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName+ '/'+ @BakFileName + '_0.bak'''
+
+DECLARE @fileCounter integer = 1;
+
+WHILE (@fileCounter < @NumberOfBakFiles)
+BEGIN
+    SELECT @TSQL += ' , URL = ''https://' + @StorageAccountName + '.blob.core.windows.net/' + @ContainerName+ '/'+ @BakFileName + '_'+ CAST(@fileCounter as varchar(10)) + '.bak'''
+    SET @fileCounter = @fileCounter + 1
+END
+
+
+
+IF(@DestinationFolderData != '')
+BEGIN
+    SELECT @TSQL = @TSQL + ' WITH MOVE ''' + @DatabaseName + ''' TO ''' + @DestinationFolderData + @DatabaseNewName + '.mdf'''
+
+    IF(@DestinationFolderLog != '')
+    SELECT @TSQL = @TSQL + ' ,MOVE ''' + @DatabaseName + '_log'' TO ''' + @DestinationFolderLog + @DatabaseNewName + '_log.ldf'''
+
+END
+
+SELECT @TSQL = @TSQL + ', REPLACE'
+
+
+EXEC (@TSQL)
+
