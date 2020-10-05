40125bae440954d7e6ae6349e5d25b1e73a61a42
Antoine Drouhin <antoine.drouhin@gmail.com>
2020-10-05 12:12:45 +0200

    [SP] Fix describe table

--- procedures/DescribeTable.sql
+++ procedures/DescribeTable.sql
@@ -1,8 +1,9 @@
 CREATE OR ALTER PROCEDURE usp_DescribeTable
     @TableName NVARCHAR(2000)
 AS
-SELECT t.name, c.name, TYPES.name as 'type', c.max_length, c.precision, c.collation_name, c.is_nullable, c.is_computed, is_identity from sys.columns c
+SELECT t.name 'table', c.name 'column', TYPES.name as 'type', c.max_length, c.precision, c.collation_name, c.is_nullable, c.is_computed, is_identity from sys.columns c
 INNER JOIN sys.tables t on c.object_id = t.object_id
 INNER JOIN sys.types TYPES on c.system_type_id = TYPES.system_type_id
 where t.name like ('%'+@TableName+'%')
+AND TYPES.name <> 'sysname'
 ORDER BY t.name,c.name
