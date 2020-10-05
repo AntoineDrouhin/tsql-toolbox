83f68e87602f17ca421ca867ced54a67c1e767a8
Antoine Drouhin <antoine.drouhin@gmail.com>
2020-10-05 11:09:01 +0200

    [SP] Describe tables
    Add a SP to quickly describe a table

---
+++ procedures/DescribeTable.sql
@@ -0,0 +1,8 @@
+CREATE OR ALTER PROCEDURE usp_DescribeTable
+    @TableName NVARCHAR(2000)
+AS
+SELECT t.name, c.name, TYPES.name as 'type', c.max_length, c.precision, c.collation_name, c.is_nullable, c.is_computed, is_identity from sys.columns c
+INNER JOIN sys.tables t on c.object_id = t.object_id
+INNER JOIN sys.types TYPES on c.system_type_id = TYPES.system_type_id
+where t.name like ('%'+@TableName+'%')
+ORDER BY t.name,c.name
